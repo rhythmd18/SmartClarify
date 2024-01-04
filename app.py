c4d1affc0521239c0253e621bc68784143d6a91f
Rhythm Dutta <duttarhythm18@gmail.com>
2024-01-04 11:34:17 +0530

    Setup for adding chat_features

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import streamlit as st
-# from dotenv import load_dotenv
-# import os
-from langchain.prompts import PromptTemplate
 from PIL import Image
 import google.generativeai as genai
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-# load_dotenv()
 
-# os.environ['GOOGLE_API_KEY'] = os.getenv('GOOGLE_API_KEY')
+
 GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=GOOGLE_API_KEY)
 
+
 llm = genai.GenerativeModel('gemini-pro')
 llm_vision = genai.GenerativeModel('gemini-pro-vision')
+
 
 with st.sidebar:
     st.subheader("Instructions:")
@@ -22,6 +24,13 @@ with st.sidebar:
              * Upload an image of your doubt if required
              * And get your explanation instantly!""")
     subject = st.selectbox("Select your Subject", ("Physics", "Chemistry", "Maths", "Computer Science"))
+    st.empty()
+    st.markdown('***Please note:***\
+                *Being an AI-based application, \
+                responses are subject to inaccuracies and inconsistencies. \
+                Kindly review the responses carefully before using them in your work. \
+                Use this application solely for understanding complicated concepts \
+                rather than for seeking solutions to questions.*')
 
 
 st.title("➕➖SciGemini➗✖️")
@@ -29,30 +38,43 @@ st.subheader('Your Personal AI Tutor for the Sciences!')
 st.text('Powered by Google Gemini')
 
 
-prompt = PromptTemplate(
+
+if 'model' not in st.session_state:
+    st.session_state['model'] = 'gemini-pro'
+
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
+
+
+prompt_template = PromptTemplate(
     input_variables=["doubt", "subject"],
     template="""You are an expert science tutor/
-    Your name is SciGemini. You explain the concepts in simple terms that are very easy to understand (even to a 15-year old kid)./
+    Your name is SciGemini. You explain the concepts in simple terms that are very easy to understand (even to a 10-year-old kid)./
     If the question is left blank and there is no image either, ask the user to write the question./
     Spread out your response in points so that it is easy to grasp./
+    Use LaTeX for mathematical equations (use $...$ for inline math expressions)./
     If the question or the image is not related to the subject strictly,/
-    please refrain from describing anything and reject politely./
-    Ask to rephrase the question or re-upload the image accordingly/
+    please refrain from describing anything and reject politely/
+    and ask to rephrase the question or re-upload the image accordingly/
     The question is: {doubt}/
     The subject is: {subject}/"""
 )
 
 
 doubt = st.text_input("Ask your doubt...")
-message = prompt.format(doubt=doubt, subject=subject)
+message = prompt_template.format(doubt=doubt, subject=subject)
+
 
 input_type = st.radio("Select Mode of Inputting Image...", ("Upload an image", "Take a picture"))
 if input_type == "Upload an image":
     img = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+    if img:
+        st.image(img)
 else:
     img = st.camera_input("Take a picture")
 img = Image.open(img) if img else None
 submit = st.button("Submit")
+
 
 def get_response(doubt, img):
     if img:
@@ -64,6 +86,5 @@ def get_response(doubt, img):
 if submit:
     with st.spinner('Thinking...'):
         response = get_response(message, img)
-    if img:
-        st.image(img)
     st.write(response.text)
+    prompt = st.chat_input('Ask a follow-up question...')
